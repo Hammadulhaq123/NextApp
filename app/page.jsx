5a6e46c2f7255750aaebd69d7c5751ccab806964
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-14 20:40:05 +0500

    Working on the navbar created a new directory in the assets that contains our images and icons rel to the project

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,18 @@
+import Feed from "@components/Feed"
+
 
 const Home = () => {
     return (
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">
                 Discover & Share
+                <br className="md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
             </h1>
-            <br className="max-md:hidden" />
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
             <p className="desc text-center">Promptopia is an open source AI prompting tool for modern world to discover, create & share creative AI-prompts</p>
+
+            {/* Feed component */}
+            <Feed />
         </section>
     )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+'use client'
 
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 const Nav = () => {
     return (
-        <div>
+        <nav className='mb-16 pt-3 flex-between w-full'>
+            <Link href="/" className='flex gap-2 flex-center'>
+                <Image
+                    src="/assets/images/logo.svg"
+                    width={30}
+                    height={30}
+                    alt='Logo'
+                    className='object-contain'
+                />
+                <p className='logo_text'>NextApp</p>
+            </Link>
 
-        </div>
+        </nav>
     )
 }
 
